efca2d26822388b3b624841c18ef9b11520569b3
xala3pa <alvaro.salazar@ticketbis.com>
2016-11-14 23:14:27 +0100

    pangram solution using java 8 approach

--- java/pangram/src/main/java/Pangrams.java
+++ java/pangram/src/main/java/Pangrams.java
@@ -1,17 +1,17 @@
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
 public class Pangrams {
     public static boolean isPangram(String sentence) {
-        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-
-        for (char letter : alphabet) {
-            if (!containsAnyLetter(sentence, letter)) {
-                return false;
-            }
-        }
-
-        return true;
+        return isUsingAllAlphabet(sentence.toLowerCase());
     }
 
-    private static boolean containsAnyLetter(String sentence, char letter) {
-        return sentence.toLowerCase().contains(String.valueOf(letter));
+    private static boolean isUsingAllAlphabet(String lowerCaseSentence) {
+        return IntStream.rangeClosed('a', 'z').
+                allMatch(contains(lowerCaseSentence));
+    }
+
+    private static IntPredicate contains(String sentence) {
+        return letter -> sentence.indexOf(letter) > -1;
     }
 }
