a8020ed44689b0893a4073e66d0bd6904bfc07d1
xala3pa <salazar3pa@gmail.com>
2016-11-26 16:57:06 +0100

    java 8 approach

--- java/pangram/src/main/java/Pangrams.java
+++ java/pangram/src/main/java/Pangrams.java
@@ -1,17 +1,10 @@
-import java.util.function.IntPredicate;
-import java.util.stream.IntStream;
-
 public class Pangrams {
+
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
     public static boolean isPangram(String sentence) {
-        return isUsingAllAlphabet(sentence.toLowerCase());
-    }
-
-    private static boolean isUsingAllAlphabet(String lowerCaseSentence) {
-        return IntStream.rangeClosed('a', 'z').
-                allMatch(contains(lowerCaseSentence));
-    }
-
-    private static IntPredicate contains(String sentence) {
-        return letter -> sentence.indexOf(letter) > -1;
+        return ALPHABET.chars()
+                .mapToObj(letter -> (char) letter)
+                .allMatch(letter -> sentence.toLowerCase().contains(letter.toString()));
     }
 }
