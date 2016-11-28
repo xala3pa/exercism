5bf52903eceb4cf8f8b1164ef21ff1d77dacd24a
xala3pa <salazar3pa@gmail.com>
2016-11-28 22:58:03 +0100

    new iterarion 2

--- java/anagram/src/main/java/Anagram.java
+++ java/anagram/src/main/java/Anagram.java
@@ -6,37 +6,28 @@ public class Anagram {
     private String anagram;
 
     Anagram(String anagram) {
-        this.anagram = anagram;
+        this.anagram = anagram.toLowerCase();
     }
 
     List<String> match(List<String> wordsList) {
         return wordsList.stream()
-                .filter(word -> isAnagram(word.toLowerCase(), anagram.toLowerCase()))
+                .filter(this::isDifferentWord)
+                .filter(this::hasSameChars)
                 .collect(Collectors.toList());
     }
 
-    private static boolean isAnagram(String word, String anagram) {
-        return isSameLength(word, anagram)
-                && hasSameChars(word, anagram)
-                && !isSameWord(word, anagram);
+    private boolean isDifferentWord(String word) {
+        return !word.toLowerCase().equals(anagram);
     }
 
-    private static boolean isSameWord(String word, String anagram) {
-        return word.equals(anagram);
+    private boolean hasSameChars(String word) {
+        return toOrderedString(word.toLowerCase()).equals(toOrderedString(anagram));
     }
 
-    private static boolean isSameLength(String word, String anagram) {
-        return word.length() == anagram.length();
-    }
-
-    private static boolean hasSameChars(String word, String anagram) {
+    private String toOrderedString(String word) {
         char[] wordChars = word.toCharArray();
-        char[] anagramChars = anagram.toCharArray();
         Arrays.sort(wordChars);
-        Arrays.sort(anagramChars);
-        String orderedWord = new String(wordChars);
-        String orderedAnagram = new String(anagramChars);
 
-        return orderedWord.equals(orderedAnagram);
+        return new String(wordChars);
     }
 }
