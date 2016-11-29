afa1a7aa5c0a7a22456d66163b0989bed5889b55
xala3pa <salazar3pa@gmail.com>
2016-11-29 23:45:41 +0100

    Hamming solution java iteration 1

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,2 +1,17 @@
 public class Hamming {
+    public static int compute(String a, String a1) {
+        int differences = 0;
+
+        if (a.length() != a1.length()) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != a1.charAt(i)) {
+                differences++;
+            }
+        }
+
+        return differences;
+    }
 }
