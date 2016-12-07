ee6bba434f2d57761433831c83fada1513291648
xala3pa <salazar3pa@gmail.com>
2016-12-07 12:08:22 +0100

    Bob simple solution

--- java/bob/src/main/java/Bob.java
+++ java/bob/src/main/java/Bob.java
@@ -1,37 +1,13 @@
-import java.util.Arrays;
-import java.util.function.Predicate;
-
 public class Bob {
-
-    enum Type {
-        QUESTION(isSilent, "Fine. Be that way!"),
-        YELL(isYelling, "Whoa, chill out!"),
-        NOTHING(isQuestion, "Sure."),
-        WHATEVER(sentence -> true, "Whatever.");
-
-        Predicate<String> predicate;
-        String sentence;
-
-        Type(Predicate<String> predicate, String sentence) {
-            this.predicate = predicate;
-            this.sentence = sentence;
+    public String hey(String sentence) {
+        if (sentence.toUpperCase().equals(sentence) && sentence.matches(".*[A-Za-z].*")) {
+            return "Whoa, chill out!";
+        } else if (sentence.endsWith("?")) {
+            return "Sure.";
+        } else if (sentence.trim().isEmpty()) {
+            return "Fine. Be that way!";
+        } else {
+            return "Whatever.";
         }
     }
-
-    private static Predicate<String> isSilent = sentence -> sentence.trim().isEmpty();
-
-    private static Predicate<String> isYelling = sentence -> sentence.matches(".*[a-zA-Z]+.*")
-            && sentence.toUpperCase().equals(sentence);
-
-    private static Predicate<String> isQuestion = sentence -> sentence.endsWith("?");
-
-    public String hey(String sentence) {
-
-
-        return Arrays.stream(Type.values())
-                .filter(e -> e.predicate.test(sentence))
-                .findFirst()
-                .get().sentence;
-    }
-
 }
