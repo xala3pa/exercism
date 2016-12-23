72e664f94bfd7b888b94d181393a55e4b7945ab0
xala3pa <salazar3pa@gmail.com>
2016-12-23 18:16:18 +0100

    space-age kata

---
+++ java/space-age/src/main/java/SpaceAge.java
@@ -0,0 +1,43 @@
+public class SpaceAge {
+    private long seconds;
+
+    public SpaceAge(long seconds) {
+        this.seconds = seconds;
+    }
+
+    public long getSeconds() {
+        return seconds;
+    }
+
+    public long onEarth() {
+        return 0;
+    }
+
+    public long onMercury() {
+        return 0;
+    }
+
+    public long onVenus() {
+        return 0;
+    }
+
+    public long onMars() {
+        return 0;
+    }
+
+    public long onJupiter() {
+        return 0;
+    }
+
+    public long onSaturn() {
+        return 0;
+    }
+
+    public long onNeptune() {
+        return 0;
+    }
+
+    public long onUranus() {
+        return 0;
+    }
+}
