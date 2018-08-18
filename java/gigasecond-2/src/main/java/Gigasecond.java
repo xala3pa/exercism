543e0e318c50566047e7aa9533afdcc0b38ccf28
xala3pa <salazar3pa@gmail.com>
2018-08-18 12:08:21 +0200

    gigasecond improved version

--- java/gigasecond-2/src/main/java/Gigasecond.java
+++ java/gigasecond-2/src/main/java/Gigasecond.java
@@ -1,14 +1,13 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 class Gigasecond {
 
-  private static final int GIGASECONDS = 1000_000_000;
+  private static final int GIGASECONDS = 1_000_000_000;
   private final LocalDateTime localdateTime;
 
   Gigasecond(LocalDate birthDate) {
-    localdateTime = LocalDateTime.of(birthDate, LocalTime.of(0, 0, 0)).plusSeconds(GIGASECONDS);
+    this(birthDate.atStartOfDay());
   }
 
   Gigasecond(LocalDateTime birthDateTime) {
