11ae8dec6da99a05e37fcd31a437f2f3e2f8da89
xala3pa <salazar3pa@gmail.com>
2020-08-18 19:01:01 +0200

    go hamming distance solution

---
+++ go/hamming/hamming.go
@@ -0,0 +1,21 @@
+// Package hamming : contain functions to calculate the Hamming Distance between two DNA strands.
+package hamming
+
+import "errors"
+
+// Distance : Calculate the Hamming Distance between two DNA strands.
+func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return -1, errors.New("String has different lengths")
+	}
+
+	distance := 0
+	target := []rune(b)
+	for i, c := range a {
+		if c != target[i] {
+			distance++
+		}
+	}
+
+	return distance, nil
+}
