d684eb9608f635c91b967bcbb4fc9dc28b7caf77
xala3pa <salazar3pa@gmail.com>
2020-08-25 22:31:51 +0200

    function created

---
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -0,0 +1,7 @@
+// Package collatzconjecture : contains functions to solve 3x+1 problem
+package collatzconjecture
+
+// CollatzConjecture : function to solve  3x+1 problem
+func CollatzConjecture(number int) (int, error) {
+	return 0, nil
+}
