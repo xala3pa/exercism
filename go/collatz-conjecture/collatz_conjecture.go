7e165460464bd16d35d8cd12cf89e46db45802fc
xala3pa <salazar3pa@gmail.com>
2020-08-26 23:07:52 +0200

    go collatz_conjecture solution

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,7 +1,27 @@
 // Package collatzconjecture : contains functions to solve 3x+1 problem
 package collatzconjecture
 
-// CollatzConjecture : function to solve  3x+1 problem
+import (
+	"errors"
+)
+
+// CollatzConjecture : function to solve 3x+1 problem
 func CollatzConjecture(number int) (int, error) {
-	return 0, nil
+	if number < 1 {
+		return -1, errors.New("function doesn't work with a negative or zero number")
+	}
+
+	counter := 0
+	for ;  number > 1; number = step(number) {
+		counter++
+	}
+
+	return counter, nil
+}
+
+func step(n int) int {
+	if n%2 == 0 {
+		return n / 2
+	}
+	return n*3 + 1
 }
