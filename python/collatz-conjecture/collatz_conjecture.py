ba30f450c0c33aa01e70e8307b868a9ec6d907b0
Xala <salazar3pa@gmail.com>
2024-01-26 13:43:59 +0100

    Python collatz-conjectur solution

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,13 @@
+def is_even(number):
+    return number % 2 == 0
+
+
 def steps(number):
-    pass
+    if number < 1:
+        raise ValueError("Only positive integers are allowed")
+
+    steps = 0
+    while number > 1:
+        number = number / 2 if is_even(number) else 3 * number + 1
+        steps += 1
+    return steps
